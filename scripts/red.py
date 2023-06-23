663eb4cf3c1c866986b545d61a98f01da0be9fde
micho137 <michaenrangelmr@gmail.com>
2023-06-23 13:17:00 -0500

    Names conditional RNN

--- scripts/red.py
+++ scripts/red.py
@@ -50,7 +50,7 @@ def preprocess_text(text):
     # Se definen los STOP WORDS excluyendo los prononmbres y terminos a utilizar 
     stop_words = set(['además', 'ante', 'bajo', 'cabe', 'con', 'contra', 'cuando', 'de', 'desde', 'donde', 'durante',
                       'e', 'a', 'el', 'en', 'entre', 'hacia', 'hasta', 'la', 'al', 'las', 'lo', 'los', 'mis', 'para',
-                      'por', 'qué', 'según', 'sin', 'sobre', 'tras', 'un', 'una','.', ','])
+                      'por', 'qué', 'según', 'sin', 'sobre', 'tras', 'un', 'una','.', ',','ser'])
 
     # POS tags permitidos (Pronombres, Verbos, Adjetivos)
     allowed_tags = ['PRON', 'VERB', 'ADJ']
@@ -64,9 +64,17 @@ def preprocess_text(text):
     for token in doc:
         # Verificar si el token no es una STOP WORD (excluyendo los pronombres) y si el POS tag se encuentra en los tags permitidos
         if token.text.lower() not in stop_words or token.pos_ in allowed_tags:
-            # Lemmatize the token
-            lemma = token.lemma_
-            processed_text.append(lemma)
+            # # Lemmatize the token
+            # lemma = token.lemma_
+            # processed_text.append(lemma)
+            if token.ent_type_ == 'PER':
+                # Leer el nombre letra por letra
+                spelled_name = list(token.text)
+                processed_text.extend(spelled_name)
+            else:
+                # Lematizar el token
+                lemma = token.lemma_
+                processed_text.append(lemma)
 
     return processed_text
 
